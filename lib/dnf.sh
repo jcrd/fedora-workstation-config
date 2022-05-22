307d278b8d8a9c047a22241aa4c7831a0d9f4e23
James Reed <james@twiddlingbits.net>
2022-05-22 12:55:32 -0600

    Reuse install functions via lib

--- lib/dnf.sh
+++ lib/dnf.sh
@@ -1,3 +1,24 @@
 dnf() {
     sudo dnf -q -y $@
 }
+
+enable_coprs() {
+    echo '--- Enabling coprs ---'
+    while read -r copr; do
+        dnf copr enable "$copr"
+    done < "$WORKDIR"/coprs.txt
+}
+
+install_packages() {
+    echo '--- Installing packages ---'
+    while read -r package; do
+        dnf install "$package"
+    done < "$WORKDIR"/packages.txt
+}
+
+install_snaps() {
+    echo '--- Installing snaps ---'
+    while read -r snap; do
+        sudo snap install $snap
+    done < "$WORKDIR"/snaps.txt
+}
