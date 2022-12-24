dd80bfd554e49a60228db327f3b7fc9e251f5672
James Reed <james@twiddlingbits.net>
2022-12-24 17:34:13 -0500

    Install syncthing
    Implement enabling of systemd system and user units.

--- lib/systemd.sh
+++ lib/systemd.sh
@@ -1,6 +1,18 @@
-enable_units() {
-    echo '--- Enabling systemd units ---'
+enable_system_units() {
+    echo '--- Enabling systemd system units ---'
     while read -r unit; do
-        sudo systemctl enable "$unit"
-    done < "$WORKDIR"/units.txt
+        sudo systemctl enable --now "$unit"
+    done < "$WORKDIR"/system_units.txt
+}
+
+enable_user_units() {
+    echo '--- Enabling systemd user units ---'
+    while read -r unit; do
+        systemctl --user enable --now "$unit"
+    done < "$WORKDIR"/user_units.txt
+}
+
+enable_units() {
+    [ -e "$WORKDIR"/system_units.txt ] && enable_system_units
+    [ -e "$WORKDIR"/user_units.txt ] && enable_user_units
 }
